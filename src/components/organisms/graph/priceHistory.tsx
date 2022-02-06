eac5e00aed4b30d02041f3f0b3c6e5cd16631b7e
Mon <mrmon93+github@gmail.com>
2022-02-07 03:37:58 +0900

    refactor: デフォルトプロパティなど整理

--- src/components/organisms/graph/priceHistory.tsx
+++ src/components/organisms/graph/priceHistory.tsx
@@ -18,24 +18,18 @@ export const PriceHistoryGraph = (data: Price[]) => {
   return (
     <ResponsiveContainer>
       <LineChart data={data}>
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="Date" />
+        <CartesianGrid strokeDasharray="3" />
+        <XAxis dataKey="Date" minTickGap={30} />
         <YAxis />
         <Tooltip formatter={dataFormatter} />
         <Legend />
-        <Line
-          dataKey="Value"
-          name="ETF Value"
-          type="linear"
-          stroke="#003f5c"
-          dot={false}
-        />
+        <Line dataKey="Value" name="ETF Value" stroke="#003f5c" dot={false} />
         <Line
           dataKey="MA"
           name="MA"
-          type="linear"
           stroke="#58508d"
           dot={false}
+          strokeWidth={2}
         />
       </LineChart>
     </ResponsiveContainer>
